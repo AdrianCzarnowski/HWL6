542015eb34bd120abcbdb45f890b9285775e6df7
aczarnowski <adriannnczarnowski@gmail.com>
2022-03-24 16:33:49 +0100

    After feedback

--- src/test/java/BasePage.java
+++ src/test/java/BasePage.java
@@ -4,33 +4,38 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 public class BasePage {
+    private static Logger logger = LoggerFactory.getLogger("BasePage.class");
 
     public WebDriver driver;
 
     @BeforeAll
     static void beforeAll() {
         WebDriverManager.chromedriver().setup();
+        logger.info("<<<<<Driver initiated properly");
     }
 
     @BeforeEach
     void beforeEach() {
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
     }
 
     @AfterEach
     void tearsDown() {
         driver.manage().deleteAllCookies();
         driver.quit();
+        logger.info("<<<<<Driver closed properly");
     }
 
     public void Test(String websiteAddress, String expectedTitle) {
         driver.get(websiteAddress);
-        driver.manage().window().maximize();
         String actualTitle = driver.getTitle();
         assertThat("Title is not correct", actualTitle, equalTo(expectedTitle));
 //        driver.switchTo().newWindow(WindowType.TAB);
--- src/test/java/WebsiteRegressionTest.java
+++ src/test/java/WebsiteRegressionTest.java
@@ -1,21 +1,29 @@
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class WebsiteRegressionTest extends BasePage {
+
+    private static Logger logger = LoggerFactory.getLogger("WebsiteRegressionTest.class");
+
 
     @Tag("Regression")
     @ParameterizedTest(name = "{0}")
     @CsvSource({
 //            "https://www.onet.pl/, Onet – Jesteś na bieżąco",
             "http://kotuszkowo.pl/, Kotuszkowo- blog o kotach",
-            "https://www.filmweb.pl/, Filmweb - filmy takie jak Ty!",
-            "https://www.selenium.dev/documentation/en/webdriver/, WebDriver | Selenium",
-            "https://siiportal.sii.pl/, Logowanie na koncie",
+//            "https://www.filmweb.pl/, Filmweb - filmy takie jak Ty!",
+//            "https://www.selenium.dev/documentation/en/webdriver/, WebDriver | Selenium",
+//            "https://siiportal.sii.pl/, Logowanie na koncie",
     })
-    public void TestRegression(String websiteAddress, String expectedTitle) {
+    void TestRegression(String websiteAddress, String expectedTitle) {
         Test(websiteAddress, expectedTitle);
+
+        logger.info("<<<<<App url : " + websiteAddress);
+        logger.info("<<<<<Expected tittle  : " + expectedTitle);
     }
 }
 
