1d5964365096f8432f64c09e50f90222ddafaa52
aczarnowski <adriannnczarnowski@gmail.com>
2022-03-25 13:17:26 +0100

    colors

--- src/test/java/WebsiteRegressionTest.java
+++ src/test/java/WebsiteRegressionTest.java
@@ -1,9 +1,11 @@
+import base.BasePage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 
 
 public class WebsiteRegressionTest extends BasePage {
@@ -17,10 +19,11 @@ public class WebsiteRegressionTest extends BasePage {
     @DisplayName("Website title regression test")
 
     void TestRegression(String websiteAddress, String expectedTitle) {
+        logger.info(BLACK +WHITE_BACKGROUND+"♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛"+COLOR_RESET);
+        logger.info(PURPLE + "<<<<<App url: " + websiteAddress);
+        logger.info(PURPLE + "<<<<<Expected tittle: " + COLOR_RESET + expectedTitle);
         Test(websiteAddress, expectedTitle);
 
-        logger.info("<<<<<App url : " + websiteAddress);
-        logger.info("<<<<<Expected tittle  : " + expectedTitle);
     }
 }
 
--- src/test/java/base/BasePage.java
+++ src/test/java/base/BasePage.java
@@ -1,3 +1,5 @@
+package base;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -14,11 +16,17 @@ public class BasePage {
     private static Logger logger = LoggerFactory.getLogger("BasePage.class");
 
     public WebDriver driver;
+    public static final String PURPLE = "\u001B[35m";
+    public static final String COLOR_RESET = "\u001B[0m";
+    public static final String WHITE_BACKGROUND = "\u001B[47m";
+    public static final String BLACK = "\u001B[30m";
 
     @BeforeAll
     static void beforeAll() {
         WebDriverManager.chromedriver().setup();
+        logger.info(BLACK +WHITE_BACKGROUND+"♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛"+COLOR_RESET);
         logger.info("<<<<<Driver initiated properly");
+        logger.info(BLACK +WHITE_BACKGROUND+"♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛"+COLOR_RESET);
     }
 
     @BeforeEach
@@ -33,13 +41,14 @@ public class BasePage {
         logger.info("<<<<<Driver deleted cookies properly");
         driver.quit();
         logger.info("<<<<<Driver closed properly");
+        logger.info(BLACK +WHITE_BACKGROUND+"♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛♛"+COLOR_RESET);
     }
 
     public void Test(String websiteAddress, String expectedTitle) {
         driver.get(websiteAddress);
         logger.info("<<<<<Driver get correct website address");
         String actualTitle = driver.getTitle();
-        logger.info("<<<<<Driver get correct website title");
+        logger.info("<<<<<Driver get actual website title : " + actualTitle);
         assertThat("Title is not correct", actualTitle, equalTo(expectedTitle));
         logger.info("<<<<<expected title is equal than actual title");
     }
